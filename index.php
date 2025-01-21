e215d1ff210035bcc5bd88f86ca3c98f04869e26
nuttachas <ing095185@gmail.com>
2025-01-21 11:36:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+	session_start();
+	$host = "demosql-7ea2ab1fc8-wpdbserver.mysql.database.azure.com";
+	$username = "kcokhgqjfd";
+	$password = "nu64311591.";
+	$database = "nut";
+
+	$mysqli = new mysqli("localhost", "root", "", "ssp");
+	if ($mysqli->connect-error) {
+		echo "Failed to connect to MySQL" (" . $mysqli->connect-error . ")" . $mysqli->connect-error;)
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
